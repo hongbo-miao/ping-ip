e058d205d483f8a63a7ae2269905a91747f27bff
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-06-22 23:26:09 -0700

    feat: multiprocessing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import subprocess
+import ipaddress
+from multiprocessing import Pool, freeze_support
 
 
 def ping(ip):
@@ -6,4 +8,31 @@ def ping(ip):
     if retval == 0:
         return True  # host is up
     else:
-        return False # host is down
+        return False  # host is down
+
+
+def ping_ips(ip1, ip2):
+    res1 = ping(ip1)
+    res2 = ping(ip2)
+    print(ip1, ip2)
+    if res1 and not res2:
+        return ip1
+    elif res2 and not res1:
+        return ip2
+    else:
+        return None
+
+
+def main():
+    ip_list = zip(
+        map(lambda ip: str(ip), list(ipaddress.ip_network('192.168.1.0/24').hosts())),
+        map(lambda ip: str(ip), list(ipaddress.ip_network('192.168.2.0/24').hosts())),
+    )
+    with Pool() as pool:
+        res = pool.starmap(ping_ips, ip_list)
+        print(res)
+
+
+if __name__ == "__main__":
+    freeze_support()
+    main()
