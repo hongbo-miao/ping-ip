ed081080e3f2118815824ded73a27e3451a0fedc
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-06-23 14:19:44 -0700

    perf: multiple threads

--- reactive.py
+++ reactive.py
@@ -5,6 +5,7 @@ import ipaddress
 import multiprocessing
 import reactivex as rx
 import subprocess
+import numpy as np
 
 
 def ping(ip):
@@ -45,15 +46,17 @@ if __name__ == "__main__":
     optimal_thread_count = multiprocessing.cpu_count()
     pool_scheduler = ThreadPoolScheduler(optimal_thread_count)
 
-    ip_lists = zip(
+    ip_tuple_list = list(zip(
         map(lambda ip: str(ip), list(ipaddress.ip_network('192.168.33.0/24').hosts())),
         map(lambda ip: str(ip), list(ipaddress.ip_network('192.168.34.0/24').hosts())),
-    )
+    ))
+    ip_tuple_list_chunks = np.array_split(ip_tuple_list, optimal_thread_count)
 
-    rx.of(*ip_lists).pipe(
-        ops_ping(), ops.subscribe_on(pool_scheduler),
-        ops.filter(lambda ip: ip),
-    ).subscribe(
-        on_next=lambda i: print("PROCESS 3: {0} {1}".format(current_thread().name, i)),
-        on_error=lambda e: print(e),
-    )
+    for ip_tuple_list_chunk in ip_tuple_list_chunks:
+        rx.of(*ip_tuple_list_chunk).pipe(
+            ops_ping(), ops.subscribe_on(pool_scheduler),
+            ops.filter(lambda ip: ip),
+        ).subscribe(
+            on_next=lambda ip: print(f"{current_thread().name} {ip}"),
+            on_error=lambda e: print(e),
+        )
